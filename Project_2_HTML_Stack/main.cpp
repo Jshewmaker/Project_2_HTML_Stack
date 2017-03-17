ef07844e15387ce131e6a4c611c24a62fcbf7af0
Joshua Shewmaker <joshuashewmaker@Joshuas-MacBook-Pro.local>
2017-03-17 12:25:15 -0400

    Initial Commit

---
+++ Project_2_HTML_Stack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project_2_HTML_Stack
+//
+//  Created by Joshua Shewmaker on 3/17/17.
+//  Copyright © 2017 Joshua Shewmaker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
